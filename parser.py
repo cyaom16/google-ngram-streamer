b59c95d7e1d39e418cbd2c20c2349e29fe46e464
Chen Yao <cyaom16@gmail.com>
2017-11-16 22:16:00 -0500

    Add kill signal handler, update logging scheme

--- parser.py
+++ parser.py
@@ -1,10 +1,21 @@
 # from time import sleep
-from util import *
+from util import get_indices, NgramStreamer, KillerHandler
 import os.path
-# import sys
+import sys
 import csv
 import re
 
+
+LOG_FILE = "./log.txt"
+
+
+def write_log(file, index, line):
+    with open(file, 'a') as f:
+        f.write(" ".join((index, str(line))) + "\n")
+    return
+
+
+killer = KillerHandler()
 
 """
      Labour party
@@ -50,63 +61,84 @@ target_dict = {'labour': ('labour party',),
 csv_template = "./ngram_match/ngram_match_{group}.csv"
 csv_header = ['ngram', 'year', 'match_count', 'volume_count']
 
-indices = None
 log_indices = []
 log_line = 0
-log_file = "./log.txt"
-if os.path.isfile(log_file):
-    with open(log_file, 'r') as f:
+
+if os.path.isfile(LOG_FILE):
+    print("Log found:\n")
+    with open(LOG_FILE, 'r') as f:
         lines = [line.strip('\n').split() for line in f.readlines()]
 
-    # Extract unique indices from the log file
-    seen = set()
-    seen_add = seen.add
-    log_indices = [line[0] for line in lines if not (line[0] in seen or seen_add(line[0]))]
-    indices = [i for i in get_indices(n=5) if i not in log_indices[:-1]]
-
     if len(lines):
+        # Extract unique indices from the log file
+        seen = set()
+        seen_add = seen.add
+        log_indices = [line[0] for line in lines if not (line[0] in seen or seen_add(line[0]))]
+        print("Indices logged:", log_indices)
         log_line = int(lines[-1][-1])
+        print("Last logged index: {} at line: {}".format(log_indices[-1], log_line))
+    else:
+        print("Empty log.")
+else:
+    print("No log exists.")
 
-    if log_line > 0:
-        print("Last logged line number", log_line)
-
-# Remove POS tags in the text
+# Remove POS tags in the n-gram text
 pos_tags = ['ADJ', 'ADP', 'ADV', 'CONJ', 'DET', 'NOUN', 'NUM', 'PRON', 'PRT', 'VERB']
 remove = '|'.join(pos_tags)
 pa = re.compile(r'\b(' + remove + r')\b\s*')
 
-print("Indices:", indices)
+# Must include the last logged index in indices to-do
+indices = [i for i in get_indices(n=5) if i not in log_indices[:-1]]
+print("Indices to-do:", indices)
+
 streamer = NgramStreamer(lang='eng-us', n=5, ver='20120701', idx=indices, stream=False)
 
+prev_index = log_indices[-1]
+prev_line = log_line
 for meta, record in streamer.iter_collection():
+    curr_line = record.line_number
+    curr_index = meta[-1]
+
     # Skipping records until the last logged if necessary
-    num = record.line_number
-    if len(log_indices) and log_line and meta[-1] == log_indices[-1] and num <= log_line:
+    if len(log_indices) > 0 and log_line > 0:
+        if curr_index == log_indices[-1] and curr_line <= log_line:
             continue
+
+    # Log the previous index and its last line (total lines)
+    if curr_index != prev_index and curr_line == 1:
+        print("Previous index {} completed with total {} records.".format(prev_index, prev_line))
+        write_log(LOG_FILE, prev_index, prev_line)
 
     s = record.ngram
     res = pa.sub('', s.replace('_X', '').replace('_', '')).lower()
 
     for key in target_dict:
-        # Log ngram match in the given group
+        # Log ngram match in each given group
         csv_file = csv_template.format(group=key).replace(' ', '_')
         file_exists = os.path.isfile(csv_file)
         with open(csv_file, 'a', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=csv_header, delimiter='\t')
             if not file_exists:
                 writer.writeheader()
-
             for tag in target_dict[key]:
                 if res.startswith(tag) or res.endswith(tag):
                     writer.writerow({'ngram': res,
                                      'year': record.year,
                                      'match_count': record.match_count,
                                      'volume_count': record.volume_count})
+    # Log rate: every 10000 lines
+    if curr_line % 10000 == 1:
+        print("Index: {}, processed {}.".format(curr_index, curr_line))
+        write_log(LOG_FILE, curr_index, curr_line)
 
-    if num % 5000 == 1:
-        print("Index: {}, processed {}.".format(meta[-1], num))
-        with open(log_file, 'a') as f:
-            # The last entry in the meta contains the index
-            f.write(" ".join((meta[-1], str(num))) + "\n")
+    # Kill signal handler interrupt, write log upon received
+    if killer.kill_now:
+        print("Kill received. Index: {} line: {}.".format(curr_index, curr_line))
+        write_log(LOG_FILE, curr_index, curr_line)
+        sys.exit(0)
+
+    prev_index = curr_index
+    prev_line = curr_line
 
 print("Complete!")
+
--- util.py
+++ util.py
@@ -1,17 +1,15 @@
 from string import ascii_lowercase, digits
 from collections import namedtuple
 from itertools import product
-# from time import sleep
 import requests
 import os.path
+import signal
 import zlib
-# import sys
 
 
 def get_indices(n=1):
     """
     Get the list of indices for Google Ngram Viewer
-
 
     # Arguments
         n: N-gram size
@@ -35,7 +33,7 @@ def get_indices(n=1):
     return sorted(list(digits) + letters + others)
 
 
-def iter_content(file, chunk_size=1024 ** 2):
+def iter_content(file, chunk_size=1024**2):
     f = open(file, 'rb')
     buffer = f.read(chunk_size)
     while buffer:
@@ -50,75 +48,88 @@ class NgramStreamer(object):
     def __init__(self, lang='eng-us', n=1, ver='20120701', idx=None, stream=True):
         """
             Ngram streamer
-            yield Record(line_number, ngram, year, match_count, volume_count)
+            Generate Record(line_number, ngram, year, match_count, volume_count)
 
             # Arguments
-                lang: Language
-                n:    N-gram size
-                ver:  Version
-                idx:  Index, or indices
+                lang: Language (str)
+                n:    N-gram size (int)
+                ver:  Version (str)
+                idx:  Index, or indices (list)
 
             # Outputs
-                yield meta, Record
+                yield meta (list), Record (namedtuple)
         """
         self.language = lang
         self.ngram_size = n
         self.version = ver
         self.indices = idx
         self.stream = stream
-
         self.Record = namedtuple('Record', ['line_number', 'ngram', 'year', 'match_count', 'volume_count'])
 
     def iter_index(self):
-        self.indices = get_indices(n=self.ngram_size) if self.indices is None else self.indices
+        session = requests.Session()
 
+        self.indices = get_indices(n=self.ngram_size) if self.indices is None else self.indices
         url_template = 'http://storage.googleapis.com/books/ngrams/books/{}'
         file_template = 'googlebooks-{lang}-all-{n}gram-{ver}-{idx}.gz'
-
-        session = requests.Session()
         for idx in self.indices:
             file = file_template.format(lang=self.language, n=self.ngram_size, ver=self.version, idx=idx)
             url = url_template.format(file)
             meta = [self.language, self.ngram_size, self.version, idx]
 
-            print("Downloading {}...".format(url))
+            print("Establish connection...")
             try:
                 req = session.get(url, stream=self.stream)
                 assert req.status_code == 200
+
                 yield file, meta, req
+
             except AssertionError:
-                print("Unable to connect to {}".format(url))
+                print("Unable to connect to {}...".format(url))
                 continue
 
-    def iter_collection(self, chunk_size=1024 ** 2):
+    def iter_collection(self, chunk_size=1024**2):
         for file, meta, req in self.iter_index():
             if self.stream:
                 compressed_chunks = req.iter_content(chunk_size=chunk_size)
             else:
                 data_path = os.path.join("./data", file)
                 if not os.path.isfile(data_path):
+                    print("Data not found, downloading {}...".format(file))
                     with open(data_path, 'wb') as f:
                         f.write(req.content)
                 compressed_chunks = iter_content(data_path, chunk_size=chunk_size)
 
             dec = zlib.decompressobj(32 + zlib.MAX_WBITS)
             last = b''
-            line_num = 0
+            num = 0
             for chunk in compressed_chunks:
                 block = dec.decompress(chunk)
-
                 lines = (last + block).split(b'\n')
                 lines, last = lines[:-1], lines[-1]
-
                 for line in lines:
                     decoded_line = line.decode('utf-8')
                     data = decoded_line.split('\t')
                     if len(data) != 4:
                         print("Ngram data less than 4 fields!")
                         continue
-                    line_num += 1
-                    yield meta, self.Record(line_num, data[0], *map(int, data[1:]))
+                    num += 1
+
+                    yield meta, self.Record(num, data[0], *map(int, data[1:]))
 
             if last:
                 print("Decompression abnormal")
                 continue
+
+
+class KillerHandler:
+    def __init__(self):
+        """
+            System Signal handler
+        """
+        self.kill_now = False
+        signal.signal(signal.SIGINT, self.signal_handler)
+        signal.signal(signal.SIGTERM, self.signal_handler)
+
+    def signal_handler(self, signum, frame):
+        self.kill_now = True
