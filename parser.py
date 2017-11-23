0d2a31b90e15a8d639e13ffab0e0b0991e268534
Chen Yao <cyaom16@gmail.com>
2017-11-23 00:00:37 -0500

    fixed result_dict initialization causes key value sets pointing to the hashcode

--- parser.py
+++ parser.py
@@ -31,8 +31,8 @@ def save_checkpoint(match_dict, log_fname, index, line):
                                          'volume_count': match_record[3]})
             match_set.clear()
 
-    with open(log_fname, 'a') as log_file:
-        log_file.write(" ".join((index, str(line))) + "\n")
+    with open(log_fname, 'a', newline='') as log_file:
+        log_file.write(" ".join((index, str(line))))
 
 
 killer = KillerHandler()
@@ -79,7 +79,7 @@ target_dict = {'labour': ('labour party',),
                'protectionism': ('protectionism', 'protectionist', 'protectionists')}
 
 # Initialize a set collection of match results
-result_dict = dict.fromkeys(target_dict, set())
+result_dict = {key: set() for key in target_dict}
 
 
 # Log parser
@@ -95,9 +95,9 @@ if os.path.isfile(LOG_FILE):
         seen = set()
         seen_add = seen.add
         log_indices = [line[0] for line in lines if not (line[0] in seen or seen_add(line[0]))]
-        print("Indices logged:\n", log_indices)
+        print("Indices logged:", log_indices)
         log_line = int(lines[-1][-1])
-        print("Last record: index '{}', line {}.\n".format(log_indices[-1], log_line))
+        print("Last record: Index '{}' Line {}".format(log_indices[-1], log_line))
     else:
         print("Empty log.")
 else:
@@ -109,29 +109,37 @@ pa = re.compile(r'_[^\s]+')
 
 # Must include the last logged index in indices to-do
 indices = [index for index in get_indices(n=5) if index not in log_indices[:-1]]
-print("Indices to-do:\n", indices)
+print("Indices to-do from: '{}' to '{}'.".format(indices[0], indices[-1]))
 
 streamer = NgramStreamer(lang='eng-us', n=5, ver='20120701', idx=indices, stream=False)
 
-prev_index = log_indices[-1]
+prev_index = indices[0]
 prev_line = log_line
+
+offset = True if (len(log_indices) > 0) and (log_line > 0) else False
+if offset:
+    print("Jumping records...")
+
 try:
-    for meta, record in streamer.iter_collection():
+    for curr_index, record in streamer.iter_collection():
         curr_line = record.line
-        curr_index = meta[-1]
+        # curr_index = meta[-1]
 
         # Skipping records until the last logged if necessary
-        if len(log_indices) > 0 and log_line > 0:
-            if curr_index == log_indices[-1] and curr_line <= log_line:
-                continue
+        if offset and (curr_index == log_indices[-1]) and (curr_line <= log_line):
+            continue
 
         # Save checkpoint at transition
-        if curr_index != prev_index and curr_line == 1:
+        if (curr_index != prev_index) and (curr_line == 1):
             save_checkpoint(result_dict, LOG_FILE, prev_index, prev_line)
-            print("Index '{}' completed with total {} records.".format(prev_index, prev_line))
+            print("Index '{}' completed with {} records.".format(prev_index, prev_line))
 
         # Modify record in tuple for hashing
-        rec = (pa.sub('', record.ngram).lower(), record.year, record.match_count, record.volume_count)
+        record_ngram = pa.sub('', record.ngram).strip()
+        if not record_ngram:
+            continue
+
+        rec = (record_ngram.lower(), record.year, record.match_count, record.volume_count)
 
         # Looking for match targets
         for group in target_dict:
@@ -142,22 +150,22 @@ try:
         # Save checkpoint every 500,000 lines
         if curr_line % 500000 == 1:
             save_checkpoint(result_dict, LOG_FILE, curr_index, curr_line)
-            print("Index: '{}', processed {}.".format(curr_index, curr_line))
+            print("Index '{}' Line {}.".format(curr_index, curr_line))
 
         # Kill signal handler
         if killer.kill_now:
             save_checkpoint(result_dict, LOG_FILE, curr_index, curr_line)
-            print("Kill received. Index: '{}', line: {}.".format(curr_index, curr_line))
+            print("Kill received. Index '{}' Line {}.".format(curr_index, curr_line))
             sys.exit(0)
 
         prev_index = curr_index
         prev_line = curr_line
 
-    print("Complete! Index '{}' with total {} records".format(prev_index, prev_line))
+    print("Complete! Index '{}' with {} records".format(prev_index, prev_line))
 
 except Exception as e:
-    print("\n--> Error: {}."
-          "\nCheckpoint saved. Index: '{}', line: {}.".format(e.args[0], prev_index, prev_line))
+    print("\n--> Error:", e.args[0])
+    print("Checkpoint saved. Index '{}' Line {}.".format(prev_index, prev_line))
     pass
 
 save_checkpoint(result_dict, LOG_FILE, prev_index, prev_line)
